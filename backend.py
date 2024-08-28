7d6e0d92cea3e7b7136ed099fac895a94dcd67cf
Elliot S. <Elliot.sequeira@gmail.com>
2024-08-28 12:12:10 -0600

    Working ec2 deploy script, updates to backend.py, server.py (#2)
    * Update server.py
    
    Test GH Action, deploy to EC2
    
    * Update server.py
    
    * Update home.html
    
    * Update home.html
    
    new ec2 host
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update home.html
    
    * Update home.html
    
    * Update backend.py
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update backend.py
    
    * Update home.html
    
    * Update home.html
    
    * Update backend.py
    
    * Update backend.py
    
    * Update backend.py
    
    * Update backend.py
    
    * Update backend.py
    
    * Update backend.py
    
    * Update backend.py

--- backend.py
+++ backend.py
@@ -6,22 +6,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def domain_name_generator(niche):
-    llm = OpenAI()
+    try:
+        llm = OpenAI()
+        output_parser = CommaSeparatedListOutputParser()
+        format_instruction = output_parser.get_format_instructions()
 
-    output_parser = CommaSeparatedListOutputParser()
-    format_instruction = output_parser.get_format_instructions()
-
-    prompt_template = PromptTemplate(
-        template="""You are SEO expert having 10 years of experience.
+        prompt_template = PromptTemplate(
+            template="""You are SEO expert having 10 years of experience.
                                     Suggest me 3 SEO optimized .com domain names for my blog in niche {niche} \n {format_instruction}""",
-        input_variables=["niche"],
-        partial_variables={"format_instruction": format_instruction},
-    )
+            input_variables=["niche"],
+            partial_variables={"format_instruction": format_instruction},
+        )
 
-    query = prompt_template.format(niche=niche)
+        query = prompt_template.format(niche=niche)
 
-    res = output_parser.parse(llm(query))
-    print(res)
-    return res
+        res = output_parser.parse(llm(query))
+        print(res)
+        return res
+    except Exception as e:
+        err = [{"error": f"An error occurred: {e}"}] 
+        print(err)
+        return err
